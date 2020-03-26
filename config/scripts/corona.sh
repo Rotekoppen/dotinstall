afddcfed56b9a19a375befa6b21d4f715460a34d
Rotekoppen <33429716+Rotekoppen@users.noreply.github.com>
2020-03-26 19:17:53 +0100

    Updated to suit my personal use

--- config/scripts/corona.sh
+++ config/scripts/corona.sh
@@ -6,6 +6,6 @@
 grep "Norway" ~/.cache/corona |
 	sed "s/\s*//g ; s/║//g ; s/│/;/g" |
 	sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" |
-	awk -F';' '{print "  A: " $6 " R: " $4 " D: " $5 " T: " $3 "  "}'
+	awk -F';' '{print "  A: " $8 " R: " $7 " D: " $5 " T: " $3 "  "}'
 
 #Thanks to Luke Smith https://www.youtube.com/watch?v=cQ03v4d3QEo
